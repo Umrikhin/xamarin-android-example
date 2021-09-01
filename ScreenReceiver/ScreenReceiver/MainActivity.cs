47b4441665e5cde71543a7cb4bb93b5493e29ba3
Umrikhin <bono39@yandex.ru>
2021-09-01 14:13:03 +0300

    send ScreenReceiver

---
+++ ScreenReceiver/ScreenReceiver/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+
+namespace ScreenReceiver
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, ScreenListener.ScreenStateListener
+    {
+        ScreenListener mScreenListener;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            mScreenListener = new ScreenListener(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            mScreenListener.unregisterListener();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            mScreenListener.begin(this);
+
+        }
+        public void onScreenOn()
+        {
+            System.Diagnostics.Debug.WriteLine("onScreenOn");
+        }
+
+        public void onScreenOff()
+        {
+            System.Diagnostics.Debug.WriteLine("onScreenOff");
+        }
+
+        public void onUserPresent()
+        {
+            System.Diagnostics.Debug.WriteLine("onUserPresent");
+        }
+
+    }
+}
